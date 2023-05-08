52341035e2d789a4404f4d127250779035abe307
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-05-07 23:57:56 -0500

    add migrations.

--- dao/models.py
+++ dao/models.py
@@ -9,29 +9,29 @@ class User(Base):
     """ User entity model. """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(length=20), unique=True, nullable=False)
+    password = Column(String(length=100), nullable=False)
     status = Column(Enum('active', 'inactive'), nullable=False, default='active')
 
 class Operation(Base):
     """ Operation entity model. """
     __tablename__ = 'operations'
 
-    id = Column(Integer, primary_key=True)
-    type = Column(Enum('addition', 'subtraction', 'multiplication', 'division', 'square_root', 'random_string'), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String(length=20), unique=True, nullable=False)
     cost = Column(Float, nullable=False)
 
 class Record(Base):
     """ Record entity model. """
     __tablename__ = 'records'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     operation_id = Column(Integer, ForeignKey('operations.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     amount = Column(Float, nullable=False)
     user_balance = Column(Float, nullable=False)
-    operation_response = Column(String)
+    operation_response = Column(String(length=200))
     date = Column(DateTime, nullable=False)
 
     operation = relationship('Operation')
